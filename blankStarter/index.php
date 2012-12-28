a24374349830219a86d76fa08e51b9e5f857d3a1
Martin Sookael <martinsookael@gmail.com>
2012-12-28 19:20:00 +0200

    fixed .gitignore

---
+++ blankStarter/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying POST LISTING.
+ */
+
+get_header(); ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+  <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+  
+  <div class="printedContent">
+    <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
+  </div>
+    
+<?php endwhile; else: ?>
+<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
+<br /><br />
+
+<?php get_footer(); ?>
